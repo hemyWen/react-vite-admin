85eba4ae42ed09489cab7bfc059fb31ff1b43396
whm <1135063561@qq.com>
2024-11-20 11:56:48 +0800

    feat(11): :sparkles: 1111
    12

--- commitlintrc.js
+++ commitlintrc.js
@@ -2,6 +2,12 @@
 module.exports = {
 	extends: ["@commitlint/config-conventional"],
 	rules: {
+		"body-leading-blank": [2, "always"],
+		"footer-leading-blank": [1, "always"],
+		"header-max-length": [2, "always", 108],
+		"subject-empty": [2, "never"],
+		"type-empty": [2, "never"],
+		"subject-case": [0],
 		"type-enum": [
 			2,
 			"always",
@@ -127,7 +133,7 @@ module.exports = {
 		scopeOverrides: undefined,
 		defaultBody: "",
 		defaultIssues: "",
-		defaultScope: "",
-		defaultSubject: ""
+		defaultScope: ""
+		// defaultSubject: ""
 	}
 };
