59077760fc18b0f1a6cb36a20ebdf5086315e67e
whm <1135063561@qq.com>
2024-11-20 09:35:49 +0800

    feat(husky): 测试提交规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
 	root: true, // 指定这是根配置文件，用于定义javascript或者Typescript项目中的代码质量和风格的规则。
-	env: { browser: true, es2020: true }, // 指定代码的运行环境
+	env: { browser: true, es2020: true, node: true }, // 指定代码的运行环境
 	extends: [
 		// 这里是越靠后的插件配置优先级越高
 		"eslint:recommended", // eslint默认规则
